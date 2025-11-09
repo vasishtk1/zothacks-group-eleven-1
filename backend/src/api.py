ad817483ea5a1d99aae76d668812eeb732631687
AaronVillegas5 <akvilleg@uci.edu>
2025-11-08 19:37:48 -0800

    Added Form and created a single upload fastAPI function

--- backend/src/api.py
+++ backend/src/api.py
@@ -7,7 +7,7 @@ $ fastapi dev src/api.py
 
 import random,tempfile
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from pydantic import BaseModel
 from state import job_state, resume_state
 from resume_parser import returns_match_score_and_suggestions
@@ -19,23 +19,24 @@ app = FastAPI()
 
 class jobDescription(BaseModel):
     job_description: str
-@app.post("/job-description")
-async def receive_description(job:jobDescription):
-    job_state.job_text = job.job_description
-    print("Received job description: ", job_state.job_text)
-    return {"message":"Job description received"}
+# @app.post("/job-description")
+# async def receive_description(job:jobDescription):
+#     job_state.job_text = job.job_description
+#     print("Received job description: ", job_state.job_text)
+#     return {"message":"Job description received"}
 
-@app.get("/response")
-async def get_response():
-    y, suggestions = returns_match_score_and_suggestions()
-    return{"Score: ": y,
-           "Suggestions: ": suggestions}
+# @app.get("/response")
+# async def get_response():
+#     y, suggestions = returns_match_score_and_suggestions()
+#     return{"Score: ": y,
+#            "Suggestions: ": suggestions}
 
 
 
 #Receives and saves filename and data from frontend
 @app.post("/upload")
-async def upload_resume(file: UploadFile = File(...)):
+async def upload_resume(file: UploadFile = File(...),job_description: str = Form(...)):
+    job_state.job_text = job_description
     """Receive a resume PDF upload."""
     contents = await file.read()
 
@@ -44,7 +45,8 @@ async def upload_resume(file: UploadFile = File(...)):
         tmp_path = tmp.name
     resume_state.filename=tmp_path
     
-    # For now, just return a confirmation
-    return {"filename": file.filename, "size": len(contents)}
+    y, suggestions = returns_match_score_and_suggestions()
+    return {"Score ": y,
+           "Suggestions ": suggestions}
 
 
